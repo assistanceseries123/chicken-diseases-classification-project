c30fc1900b4f4f03f052b4ecc1cd3223af3ee987
Aklesh yadav <yaklesh802@gmail.com>
2024-10-15 16:39:35 +0530

    data_ingestion_added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.chicken_diseases_classification_project import logger
+from src.chicken_diseases_classification_project.components.data_ingestion import DataIngestion
+from src.chicken_diseases_classification_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("starting the chicken disease classification project")
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completd <<<<<<<<<<<<<\n\nX============X")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
